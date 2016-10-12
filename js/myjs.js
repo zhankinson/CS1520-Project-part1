1fe4ca9ac81f1da6f9167605ac4879e3df878851
zhankinson <zah15@pitt.edu>
2016-10-11 20:26:10 -0400

    Almost finished
    Fixed bug with google maps and replaced favorites grid with image
    carousel

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,13 @@
+$("#con").click(function(){
+  $("#map").css("visibility", "visible");
+});
+$("#me").click(function(){
+  $("#map").css("visibility", "hidden");
+});
+$("#you").click(function(){
+  $("#map").css("visibility", "hidden");
+});
+
 function myMap(){
   var coordinate = {lat: 40.439676, lng: -79.955299};
   var map = new google.maps.Map(document.getElementById("map"), {
