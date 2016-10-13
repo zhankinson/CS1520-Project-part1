45205bfa5043e62812b267057a997526a75c5c39
zhankinson <zah15@pitt.edu>
2016-10-13 13:58:43 -0400

    Final Commit (Hopefully)
    Fixed spacing to adhere to styleguide

--- js/myjs.js
+++ js/myjs.js
@@ -1,21 +1,29 @@
-$("#con").click(function(){
-  $("#map").css("visibility", "visible");
-});
-$("#me").click(function(){
-  $("#map").css("visibility", "hidden");
-});
-$("#you").click(function(){
-  $("#map").css("visibility", "hidden");
+$('#con').click(function() {
+    $('#map').css('visibility', 'visible');
 });
 
-function myMap(){
-  var coordinate = {lat: 40.439676, lng: -79.955299};
-  var map = new google.maps.Map(document.getElementById("map"), {
+$('#me').click(function() {
+  $('#map').css('visibility', 'hidden');
+});
+
+$('#you').click(function() {
+  $('#map').css('visibility', 'hidden');
+});
+
+function myMap() {
+  var coordinate = {
+    lat: 40.439676,
+    lng: -79.955299
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center : coordinate
-    });
+    center: coordinate
+  });
+
   var marker = new google.maps.Marker({
     position: coordinate,
     map: map
-    });
-}
+  });
+
+};
